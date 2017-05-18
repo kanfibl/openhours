d8ddbff00be1436d47c57518dea1df77f081619e
kanfibl <kanfibl@gmail.com>
2017-05-18 12:57:17 +0200

    Update do_scrape.R

--- do_scrape.R
+++ do_scrape.R
@@ -1,8 +1,7 @@
 ## do the scraping for this file
-setwd('/Users/Alex/Dropbox/openingstijden')
+#setwd('/Users/Alex/Dropbox/openingstijden')
 region_links <- readRDS("region_links.rds")
-#n = commandArgs(trailingOnly=TRUE)
+n = commandArgs(trailingOnly=TRUE)
 source("script/exec_scrape.R")
-for(n in seq(12)){
-exec_scrape(region_links,n=n)
-}
+exec_scrape(region_links,n)
+
